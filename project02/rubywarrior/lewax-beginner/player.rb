a2e73dc0104f95e77ce9dce20ee4cb329af2c778
Christopher Youden <cyouden@mines.edu>
2012-01-16 12:04:25 -0700

    finished level 4

--- project02/rubywarrior/lewax-beginner/player.rb
+++ project02/rubywarrior/lewax-beginner/player.rb
@@ -1,11 +1,19 @@
 class Player
-  def play_turn(warrior)
-    if warrior.feel(:forward).enemy?
-		warrior.attack!
-	elsif warrior.health < 20
-		warrior.rest!
-	else
-		warrior.walk!
+	def play_turn(warrior)
+		@health = 20 if @health == nil
+		
+		if warrior.feel.enemy?
+			warrior.attack!
+		elsif warrior.feel.captive?
+			warrior.rescue!
+		elsif warrior.health < @health
+			warrior.walk!(:forward)
+		elsif warrior.health < 20
+			warrior.rest!
+		else
+			warrior.walk!
+		end
+		
+		@health = warrior.health
 	end
-  end
 end
