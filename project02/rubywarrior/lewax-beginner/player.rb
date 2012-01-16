f2bcaa162f019e3e82ee4d6d8c674c332faf1b86
Christopher Youden <cyouden@mines.edu>
2012-01-16 12:18:59 -0700

    finished level 6

--- project02/rubywarrior/lewax-beginner/player.rb
+++ project02/rubywarrior/lewax-beginner/player.rb
@@ -1,19 +1,37 @@
 class Player
 	def play_turn(warrior)
 		@health = 20 if @health == nil
+		@direction = :backward if @direction == nil
 		
-		if warrior.feel.enemy?
-			warrior.attack!
-		elsif warrior.feel.captive?
-			warrior.rescue!
+		felt = warrior.feel(@direction)
+		
+		if warrior.health < 10 && felt.enemy?
+			@direction = opposite_direction(@direction)
+			warrior.walk!(@direction)
+		elsif felt.enemy?
+			warrior.attack!(@direction)
+		elsif felt.captive?
+			warrior.rescue!(@direction)
+		elsif felt.wall?
+			@direction = :forward
 		elsif warrior.health < @health
-			warrior.walk!(:forward)
+			warrior.walk!(@direction)
 		elsif warrior.health < 20
 			warrior.rest!
 		else
-			warrior.walk!
+			warrior.walk!(@direction)
 		end
 		
 		@health = warrior.health
+	end
+	
+	def opposite_direction(direction)
+		if direction == :forward
+			return :backward
+		elsif direction == :backward
+			return :forward
+		else
+			return :forward
+		end
 	end
 end
