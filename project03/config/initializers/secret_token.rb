20543bd06d10fa74ce182e3ea65afc5f1c8364ae
Christopher Youden <cyouden@mines.edu>
2012-02-05 14:56:56 -0700

    added initial project files

---
+++ project03/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project03::Application.config.secret_token = 'd39267a166fd00a6ba78f9306405ad462fc675ce7fd130ca570e452691196c42478630e773638bb1ea883219e70c777dd138f70bd29bbb5a0b742b000a3fe383'
