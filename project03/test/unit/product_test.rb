869fc86a1c27c7a72a6479e8c370c1bc3ad0cb5a
Christopher Youden <cyouden@mines.edu>
2012-02-05 17:40:06 -0700

    added a fixture, added another test, realized the ch5 i thought i finished was actually ch6...now done with ch7

--- project03/test/unit/product_test.rb
+++ project03/test/unit/product_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
+  
   test "product attributes must not be empty" do
     product = Product.new
     assert product.invalid?
@@ -42,4 +44,9 @@ class ProductTest < ActiveSupport::TestCase
     end
   end
   
+  test "product is not valid without a unique title" do
+    product = Product.new(title: products(:ruby).title, description: "yyy", price: 1, image_url: "fred.gif")
+    
+    assert !product.save
+    assert_equal "has already been taken", product.errors[:title].join('; ')
 end
