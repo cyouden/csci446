bbafea1ddab45568f753112da6d5dbad3d6147d0
Christopher Youden <cyouden@mines.edu>
2012-02-06 21:17:24 -0700

    fixed error in displaying thank you for purchase message, parts of users stuff

--- project03/app/controllers/orders_controller.rb
+++ project03/app/controllers/orders_controller.rb
@@ -53,7 +53,7 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
-        format.html { redirect_to store_url notice: 'Thank you for your order.' }
+        format.html { redirect_to store_url, notice: 'Thank you for your order.' }
         format.json { render json: @order, status: :created, location: @order }
       else
         @cart = current_cart
