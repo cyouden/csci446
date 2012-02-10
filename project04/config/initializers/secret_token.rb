fdf22070d3aa625256decec24b93f4a9163d5f1f
Christopher Youden <cyouden@mines.edu>
2012-02-10 08:06:39 -0700

    generated project04 app

---
+++ project04/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project04::Application.config.secret_token = '12b4486b27f7e79eb97c958bcc38d1bba7feec7425fb04ff12c73e35f9ec51cd0a16ed5a63fc79c547386904c0205ef0eef80a1cc4efc690be113f5beca81916'
