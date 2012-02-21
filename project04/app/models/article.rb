85da1f72be8fcc2d682e8b4382306d4edb5f712e
Christopher Youden <cyouden@mines.edu>
2012-02-20 20:40:33 -0700

    validation, forms done. most functional requiremnts are finished

--- project04/app/models/article.rb
+++ project04/app/models/article.rb
@@ -1,2 +1,7 @@
 class Article < ActiveRecord::Base
+	validates :title, :author, :body, presence: true
+	validates :author, format: {
+		with: %r{^(?:(?!Pat).)*$}i,
+		message: 'authors named Pat are not allowed'
+	}
 end
