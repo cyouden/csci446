26ca7793167518f57e6a63c6b519d5e4a59f1a35
Christopher Youden <cyouden@mines.edu>
2012-04-04 08:55:47 -0600

    generated user, role, game scaffolds

---
+++ project06/db/migrate/20120404144841_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password
+      t.string :firstname
+      t.string :lastname
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
