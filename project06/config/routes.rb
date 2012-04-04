348b6a21f3cc7ae8dedd3637eb03db0937f3d77d
Christopher Youden <cyouden@mines.edu>
2012-04-04 08:55:40 -0600

    generated user, role, game scaffolds

--- project06/config/routes.rb
+++ project06/config/routes.rb
@@ -1,4 +1,10 @@
 Gamez::Application.routes.draw do
+  resources :games
+
+  resources :roles
+
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
