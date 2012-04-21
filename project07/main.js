6259bd2efe0752e3b82bd8bc377177608618ffa9
Christopher Youden <cyouden@mines.edu>
2012-04-20 21:27:31 -0600

    functional requirements met, will add visual effects if I have time later

--- project07/main.js
+++ project07/main.js
@@ -3,9 +3,7 @@ var highScores = new Array([9, "HarryJamesPotter"], [3, "ZedCthulhu"], [2, "Near
 var goalNumber=Math.floor(Math.random()*101);
 
 $(function() {
-  $('#highOrLow').text(goalNumber);
-  updateScore(guessesLeft);
-  populateHighScores(highScores);
+  resetGame();
   
   $('form#guessTheNumber').keypress(function(e) {
     if(e.which == 13) {
@@ -15,9 +13,25 @@ $(function() {
   });
   
   $('#btnGuess').click(checkGuess);
+  
+  $('#playAgain').click(resetGame);
 });
 
+function resetGame() {
+  $('#endGameMessage').hide();
+  $('#guessTheNumber').show();
+  $('#playAgain').hide();
+  guessesLeft = 10;
+  goalNumber=Math.floor(Math.random()*101);
+  
+  updateScore(guessesLeft);
+  populateHighScores(highScores);
+  
+  $('#highOrLow').text(goalNumber);
+}
+
 function populateHighScores(scores) {
+  $('div#highScores').text('');
   for (var i = 0; i < scores.length; ++i) {
     $('div#highScores').append("<p>" + scores[i][0] + " " + scores[i][1] + "</p>");
   }
@@ -33,20 +47,36 @@ function checkGuess() {
   guessesLeft = guessesLeft - 1;
   updateScore(guessesLeft);
   
-  if (guess == goalNumber) {
-    //victory
+  if (guess < goalNumber) {
+    //too low message
+    $('#highOrLow').text('Too low! ' + goalNumber);
   }
   else if (guess > goalNumber) {
     //too high message
-    $('#highOrLow').text('Too high!');
+    $('#highOrLow').text('Too high! ' + goalNumber);
   }
   else {
-    //too low message
-    $('#highOrLow').text('Too low!');
+    //victory
+    endGame('YOU WIN!');
+    winDialog();
   }
 
   if (guessesLeft <= 0) {
     //lose
+    endGame('YOU LOSE!');
   }
+}
+
+function endGame(msg) {
+  $('#guessTheNumber').hide();
+  $('#playAgain').show();
+  $('#endGameMessage').text(msg);
+  $('#endGameMessage').slideToggle(500);
+}
+
+function winDialog() {
+  var name = window.prompt("What is your name?","");
+  highScores.push([guessesLeft, name ? name : "Guest"]);
+  populateHighScores(highScores);
 }
 
